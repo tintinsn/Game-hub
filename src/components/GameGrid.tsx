277bef18ef83fde7049c250b3baa4af9ffcb58a9
tintinsn <tin.sn98@gmail.com>
2024-02-06 16:01:36 +0700

    Build search input

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -12,7 +12,6 @@ interface Props {
 
 const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
-  console.log(data);
   const skeletons = [1, 2, 3, 4, 5, 6];
 
   return (
